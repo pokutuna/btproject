62cfa948a71378ff6c3ea6befbdc4a6ac5c5d12a
pokutuna <oneetyan@gmail.com>
2009-06-07 20:32:03 +0900

    mysqlに入れることにした

---
+++ crawler/twitter_post_db.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby -Ku
+
+require 'rubygems'
+require 'active_record'
+
+ActiveRecord::Base.default_timezone = :utc
+logger = ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'mysql', :database => 'twitter_posts', :username => 'root', :encoding => 'utf8')
+
+class Post < ActiveRecord::Base
+	def to_s
+		return "#{username}: #{message} #{time} #{uri}"
+	end
+end
+
+class Update < ActiveRecord::Base; end
+
+def create_tables
+	
+	ActiveRecord::Migration.create_table(:posts){ |t|
+		t.string :username, :null => false
+		t.string :message, :null => false
+		t.string :url, :null => false
+		t.time :time, :null => false
+	}
+
+	ActiveRecord::Migration.create_table(:updates){ |t|
+		t.time :time, :null =>false
+		t.string :action, :null
+	}
+	
+end
+
+if $0 == __FILE__ then
+	create_tables
+end
+
+
+
+
+
+
+
--- crawler/user_timeline.rb
+++ crawler/user_timeline.rb
@@ -3,6 +3,7 @@
 require 'rexml/document'
 require 'open-uri'
 
+require File.dirname(__FILE__)+'/twitter_post_db.rb'
 require File.dirname(__FILE__)+'/../mekatuna/modules/twitterstring.rb'
 
 class String; include TwitterString; end
@@ -12,41 +13,45 @@ Twitter_rss = 'http://twitter.com/statuses/user_timeline/'
 Twitter_rss_suffix = '.atom?page='
 
 user = 'pokutuna'
-File.open(user+'_posts.txt','a'){ |file|
+
+catch(:exit){
+	begin
+		doc = REXML::Document.new(open(Twitter+user).read)
+		doc.elements['//*[@id="rssfeed"]'].to_s =~ /user_timeline\/(\d+)\.rss/
+		user_id =  $1
+	rescue => e
+		puts e.class, e.message, e.backtrace
+		throw :exit
+	end
+
+	buffer404 = Array.new
+	count404 = 0
 	
-	catch(:exit){
+	for pagenum in 1..150
+		sleep 5
 		begin
-			doc = REXML::Document.new(open(Twitter+user).read)
-			doc.elements['//*[@id="rssfeed"]'].to_s =~ /user_timeline\/(\d+)\.rss/
-			user_id =  $1
-		rescue => e
-			puts e.class, e.message, e.backtrace
-			throw :exit
-		end
-
-		file.puts Time.now
-
-		buffer404 = Array.new
-		count404 = 0
-		for pagenum in 0..150
-			begin
-				doc = REXML::Document.new(open(Twitter_rss+user_id+Twitter_rss_suffix+pagenum.to_s).read.unescapeHTML)
-				count404 = 0
-				doc.elements.each('//entry') do |e|
-					text = e.elements['content'].text.sub(user+': ', '')
-					e.elements['updated'].text =~ /(\d+)\-(\d+)\-(\d+)T(\d+):(\d+):(\d+)\+(.*)/
-					time = Time.utc($1,$2,$3,$4,$5,$6).to_s
-					url =  e.elements['link[@rel="alternate"]/attribute::href'].to_s
-					file.puts "#{text},#{time},#{url}"
-				end
-				sleep 5 #wait
-			rescue => e
-				count404 += 1
-				break if count404 >= 5
-				buffer404.push(pagenum)
+			urlstr = Twitter_rss+user_id+Twitter_rss_suffix+pagenum.to_s
+			doc = REXML::Document.new(open(urlstr).read.unescapeHTML)
+			count404 = 0
+			doc.elements.each('//entry') do |e|
+				text = e.elements['content'].text.sub(user+': ', '')
+				e.elements['updated'].text =~ /(\d+)\-(\d+)\-(\d+)T(\d+):(\d+):(\d+)\+(.*)/
+				time = Time.utc($1,$2,$3,$4,$5,$6)
+				url =  e.elements['link[@rel="alternate"]/attribute::href'].to_s
+				Post.create(
+					:username => user, :message => text, :url => url, :time => time)
 			end
+			
+		rescue => e
+			count404 += 1
+			break if count404 >= 5
+			buffer404.push(pagenum)
+			p urlstr
+			p e.class.to_s + e.message.to_s + e.backtrace.to_s
 		end
-		p buffer404
-	}
-	
+	end
+	p buffer404
+	Update.create(:time => Time.new.utc, :action => 'Crawled')
 }
+
+
