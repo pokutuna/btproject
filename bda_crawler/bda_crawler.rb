b10634d899464c0800545f023a60c9eee40a95a2
pokutuna <oneetyan@gmail.com>
2009-08-10 00:10:30 +0900

    using Thread

--- bda_crawler/bda_crawler.rb
+++ bda_crawler/bda_crawler.rb
@@ -1,21 +1,63 @@
 # -*- coding: utf-8 -*-
 
 require 'date'
+require 'monitor'
 
-WAIT_TIME = 5
+WAIT_TIME = 10
 
-today = Date.today.strftime("%Y%m%d")
-File.open(today+'.txt', 'a'){ |file|
-  loop do
+class BDALogger
+  attr_accessor :today, :logfile
+
+  def initialize
+    @today = Date.today.strftime("%Y%m%d")
+    @logfile = File.open(@today+'.txt', 'a')
+    @logfile.sync = true
+    @mutex = Monitor.new
+  end
+  
+  def inquiry
     `hcitool scan`.each_line do |l|
-      data = l.scan(/([\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2})\t(.+)/).flatten
-      if data != []
-        str = Time.now.strftime('%Y/%m/%d %X')+"\t"+data[0]+"\t"+data[1]
-        file.puts str
+      data = l.scan(/((?:[\dA-F]{2}:){5}[\dA-F]{2})\t(.+)/).flatten
+      if data != [] then
+        str = Time.now.strftime('%Y/%m/%d %X')+"\t"+data.join("\t")
+        @mutex.synchronize{
+          unless @logfile.closed? then
+            @logfile.puts str
+          else
+            raise Error, "Log file #{@logfile.path} is closed."
+          end
+        }
         puts str
       end
     end
+  end
+
+  def refresh_date
+    day = Date.today.strftime("%Y%m%d")
+    if @today != day then
+      @today = day
+      close
+      @logfile = File.open(@today+'.txt', 'a')
+      @logfile.sync = true
+      true
+    else
+      false
+    end
+  end
+
+  def close
+    @mutex.synchronize{ @logfile.close}
+  end
+end
+
+logger = BDALogger.new
+
+Thread.start{
+  loop do
+    Thread.start{ logger.inquiry}
     sleep WAIT_TIME
   end
 }
 
+gets
+logger.close
