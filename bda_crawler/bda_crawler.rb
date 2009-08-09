4375687ec491e866004243513d748ff60258ab02
pokutuna <oneetyan@gmail.com>
2009-08-09 16:27:33 +0900

    untabify!!!!!!

--- bda_crawler/bda_crawler.rb
+++ bda_crawler/bda_crawler.rb
@@ -6,16 +6,16 @@ WAIT_TIME = 5
 
 today = Date.today.strftime("%Y%m%d")
 File.open(today+'.txt', 'a'){ |file|
-	loop do
-		`hcitool scan`.each_line do |l|
+  loop do
+    `hcitool scan`.each_line do |l|
       data = l.scan(/([\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2})\t(.+)/).flatten
       if data != []
-  			str = Time.now.strftime('%Y/%m/%d %X')+"\t"+data[0]+"\t"+data[1]
-				file.puts str
-				puts str
-			end
-		end
-		sleep WAIT_TIME
-	end
+        str = Time.now.strftime('%Y/%m/%d %X')+"\t"+data[0]+"\t"+data[1]
+        file.puts str
+        puts str
+      end
+    end
+    sleep WAIT_TIME
+  end
 }
 
