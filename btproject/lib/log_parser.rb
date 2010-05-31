45bec7f9cfb0fcfacf259f33d1d0415587113b6e
pokutuna <oneetyan@gmail.com>
2010-05-31 15:50:42 +0900

    created simple log_parser & pending specs

--- btproject/lib/log_parser.rb
+++ btproject/lib/log_parser.rb
@@ -3,11 +3,20 @@
 require 'date'
 require 'time'
 
-class Record
-  @date
+class BDAPair
   @name
   @bda
-  attr_reader :date, :name, :bda
+  attr_reader :name, :bda
+
+  def initialize(name, bda)
+    @name = name
+    @bda = bda
+  end
+end
+
+class Record < BDAPair
+  @date
+  attr_reader :date
 
   def initialize(line)
     ary = line.split("\t")
@@ -33,19 +42,47 @@ class Record
 end
 
 
-class DataContainer
-  @loggers
-end
-
 class Logger
+  @@threshold = 60 * 5 #TODO
   @name
-  @detect_count = Hash.new(0) #Hash bda => count
-  @last_contact = Hash.new() #Hash bda => Time
+  @detect_count
+  @meet_count
+  @last_contact
+  attr_reader :name, :detect_count, :meet_count, :last_contact
+  
+  def initialize(name)
+    @name = name 
+    @detect_count = Hash.new(0) #Hash bda => count
+    @meet_count = Hash.new(0)
+    @last_contact = Hash.new(Time.at(0)) #Hash bda => Time
+  end
+  
+  def add_record(record)
+    raise ArgumentError unless Record === record
+    @detect_count[record.bda] += 1
+    
+    diff = record.date - @last_contact[record.bda]
+    raise RuntimeError 'added older record' if diff < 0
+    @meet_count[record.bda] += 1 if diff > @@threshold
+    
+    @last_contact[record.bda] = record.date
+  end
+
+  def count_byBDA(bda)
+    return [@detect_count, @meet_count]
+  end
 end
 
 
 
-
-
+=begin
+File.open('../spec/sampledata/samplelog_day1.tsv'){ |f|
+  hoge = Logger.new('hoge')
+  f.each_line do |line|
+    hoge.add_record(Record.new(line.chomp)) unless line[0] == '#'
+  end
+  p hoge
+}
+=end
 
     
--- btproject/spec/log_parser_spec.rb
+++ btproject/spec/log_parser_spec.rb
@@ -10,26 +10,26 @@ describe Record, 'when parsing log data' do
     @sample4 = '2009/11/20	17:8:0		00:1B:DC:00:0F:5B'
   end
 
-  it "should parse a log line" do
+  it 'should parse a log line' do
     record = Record.new(@sample1)
     record.date.should == Time.local(2009,11,10,10,39,12)
     record.name.should == 'pokutuna-ThinkPad'
     record.bda.should == '00:19:7E:F6:F5:5D'
   end
 
-  it "should parse a log line without device name" do
+  it 'should parse a log line without device name' do
     record = Record.new(@sample2)
     record.name.should == ''
     record.bda.should == '00:22:F3:9C:37:D8'
   end
 
-  it "should parse string has extra TAB between day and time" do
+  it 'should parse string has extra TAB between day and time' do
     record = Record.new(@sample3)
     record.name.should == 'hoge-pc'
     record.bda.should == '00:1B:DC:00:04:18'
   end
 
-  it "should parse string has extra TAB without device name" do
+  it 'should parse string has extra TAB without device name' do
     record = Record.new(@sample4)
     record.name.should == ''
     record.bda.should == '00:1B:DC:00:0F:5B'
@@ -37,22 +37,22 @@ describe Record, 'when parsing log data' do
 
 end
 
-describe Record, "when comparing" do
+describe Record, 'when comparing' do
   before(:all) do
     @sample1 = Record.new('2009/11/8 19:13:8	pokutuna-ThinkPad	00:19:7E:F6:F5:5D')
     @sample2 = Record.new('2009/11/8 19:13:8	hoge	00:19:7E:F6:F5:5D')
     @sample3 = Record.new('2009/11/8 19:34:39	pokutuna-ThinkPad	00:19:7E:F6:F5:5D')
   end
 
-  it "should compare about same time Record object" do
+  it 'should compare about same time Record object' do
     @sample1.should == @sample2
   end
 
-  it "should compare about old time Record object" do
+  it 'should compare about old time Record object' do
     @sample1.should < @sample3
   end
 
-  it "should compare with Time object" do
+  it 'should compare with Time object' do
     @sample1.should == Time.parse('2009/11/8 19:13:8')
     @sample1.should < Time.parse('2010/11/18 19:13:8')
   end
@@ -62,4 +62,19 @@ end
 
 
 
-  
+describe Logger do
+  context 'when create new Logger object' do
+    it 'should be empty Hashes'
+    it 'should be zero, count default'
+    it 'should be Unix epoch time, last contact default'
+  end
+
+  context 'when add_record' do
+    it 'should raise ArgumentError by adding other class'
+    it 'should raise RuntimeError by adding older record'
+  end
+
+  context 'when count by BDA' do
+    it 'should return zero, unregistered BDA'
+  end
+end
