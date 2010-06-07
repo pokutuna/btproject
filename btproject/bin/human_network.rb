efb64e46972d213cb4174c8e977e4c2e94aeb68b
pokutuna <oneetyan@gmail.com>
2010-06-07 13:55:21 +0900

    with bda

--- btproject/bin/human_network.rb
+++ btproject/bin/human_network.rb
@@ -7,7 +7,7 @@ require 'log_parser'
 
 
 users = YAML.load(ARGF.read)
-
+bdas = users.map{ |k,v| v[1]} 
 loggers = []
 
 users.each{ |k,v|
@@ -15,7 +15,7 @@ users.each{ |k,v|
   logger = Logger.new(k)
 
   puts "reading log files"
-  Dir.glob(v).each do |filename|
+  Dir.glob(File.expand_path(v[0])).each do |filename|
     File.open(filename){ |f|
       puts '  '+File.basename(f.path)
       f.each_line do |line|
@@ -28,6 +28,7 @@ users.each{ |k,v|
   puts "analyzeing log"
   result = logger.analyze
   result = result.to_a.sort_by{|i| i[1][0]}.reverse
+  result = result.select{ |i| bdas.include? i[0]}
   result.each do |i|
     puts "  #{i[0]} detect:#{i[1][0]}  meet:#{i[1][1]}"
   end
