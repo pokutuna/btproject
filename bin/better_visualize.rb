c2493e83e1ee8c6689b15da85f9a0aba2e1c85dc
pokutuna <oneetyan@gmail.com>
2010-09-01 10:24:04 +0900

    better

---
+++ bin/better_visualize.rb
@@ -0,0 +1,71 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__)+'/../lib')
+
+require 'graph_parser'
+require 'graph_analyzer'
+require 'graphviz'
+
+sample = ['sep3_1030_1100', 'sep3_1100_1130', 'sep3_1230_1300']
+hci = ['okuura','ushikoshi','kono','tokuami'].map(&:to_sym)
+net = ['EEEPC06','EEEPC13','EEEPC16','eeepc04','eeepc08','eeepc11','eeepc12','eeepc15','eeepc18'].map(&:to_sym)
+
+sample.map{ |s| "./net/#{s}.txt"}.each do |path|
+  @graph = AdjacencyMatrix.open(path, :graph)
+  p path
+  
+  cliques = @graph.local_maximum_cliques
+  
+  isolated = @graph.local_maximum{ |subgraph|
+    @graph.isolated_clique?(subgraph)}
+  
+  pseudo = @graph.local_maximum{ |subgraph|
+    @graph.pseudo_clique_density?(subgraph)}
+
+  ipc = @graph.local_maximum{ |subgraph|
+    @graph.isolated_pserudo_clique_density?(subgraph)}
+  
+  @viz = GraphViz.new(:G, :type => :graph, :ratio => "compress")
+  @nodes = Hash.new #symbol => GraphViz::Node
+
+  #draw nodes with lab subgraph
+  def draw_nodes(group)
+    @viz.subgraph{ |c|
+      c[:rank => 'same']
+      group.each do |n|
+        color = 'white'
+        color = 'lightgray' if @graph.nodes_from(n).size == 0
+        @nodes[n] = @viz.add_node(n.to_s,
+          :style => "filled", :fillcolor => color)
+      end
+    }
+  end
+
+  draw_nodes(hci)
+  draw_nodes(net)
+
+  cliques = [cliques, pseudo, isolated, ipc]
+  colors = ["green", "blue", "red", "purple"]
+  clique_names = %w[cliques pseudo isolated ipc]
+
+  
+  cliques.zip(colors, clique_names).each do |pair|
+    pair[0].each_with_index do |c, idx|
+      root = @viz.add_node("#{pair[2]+idx.to_s}",
+        :shape => "diamond",
+        :style => "filled",
+        :fillcolor => pair[1])
+      root[:root] = true
+      c.each do |n|
+        @viz.add_edge(root, @nodes[n],
+          :color => pair[1],
+          :fontcolor => pair[1])
+      end
+    end
+  end
+
+  print @viz.output(:canon => String)
+  output_name = File.basename(path, ".*")
+  @viz.output(:use => :dot, :png => "./"+output_name+".png")
+  @viz.output(:use => :circo, :png => "./"+output_name+"_c.png")
+  @viz.output(:use => :twopi, :png => "./"+output_name+"_t.png")
+end
+
