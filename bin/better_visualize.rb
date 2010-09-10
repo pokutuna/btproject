724e5387656ec66eb3d726cbe2ce1cee3f7458e8
pokutuna <oneetyan@gmail.com>
2010-09-10 12:10:16 +0900

    Revert "better"
    This reverts commit c2493e83e1ee8c6689b15da85f9a0aba2e1c85dc.
    
    Conflicts:
    
    	bin/better_visualize.rb

--- bin/better_visualize.rb
+++ bin/better_visualize.rb
@@ -5,16 +5,16 @@ require 'graph_analyzer'
 require 'graphviz'
 
 sample = ['sep3_1030_1100', 'sep3_1100_1130', 'sep3_1230_1300']
-hci = ['okuura','ushikoshi','kono','tokuami']
-net = ['EEEPC06','EEEPC13','EEEPC16','eeepc04','eeepc08','eeepc11','eeepc12','eeepc15','eeepc18']
-[hci, net].each{ |strs| strs = strs.map(&:to_sym)}
+hci = ['okuura','ushikoshi','kono','tokuami'].map(&:to_sym)
+net = ['EEEPC06','EEEPC13','EEEPC16','eeepc04','eeepc08','eeepc11','eeepc12','eeepc15','eeepc18'].map(&:to_sym)
 
-sample.map{ |s| "./net/#{s}.txt"}.each do |path|
+Dir.glob('./net/*.txt').each do |path|
+#sample.map{ |s| "./net/#{s}.txt"}.each do |path|
   @graph = AdjacencyMatrix.open(path, :graph)
   p path
   
   cliques = @graph.local_maximum_cliques
-  
+ 
   isolated = @graph.local_maximum{ |subgraph|
     @graph.isolated_clique?(subgraph)}
   
@@ -24,20 +24,32 @@ sample.map{ |s| "./net/#{s}.txt"}.each do |path|
   ipc = @graph.local_maximum{ |subgraph|
     @graph.isolated_pserudo_clique_density?(subgraph)}
   
-  @viz = GraphViz.new(:G, :type => :graph, :ratio => "compress")
+  #create graphviz
+  @viz = GraphViz.new(:G, :type => :graph, :compound => true, :ratio => :compress)
   @nodes = Hash.new #symbol => GraphViz::Node
 
   #draw nodes with lab subgraph
   def draw_nodes(group, label)
-    @viz.subgraph{ |c|
-      c[:rank => 'same']
-      group.each do |n|
-        color = 'white'
-        color = 'lightgray' if @graph.nodes_from(n).size == 0
-        @nodes[n] = @viz.add_node(n.to_s,
-          :style => "filled", :fillcolor => color)
-      end
-    }
+    c = @viz.add_graph(label.to_sym)
+    c[:label] = label
+    c[:style] = "filled"
+    c[:color] = "lightgrey"
+    c[:compound] = true
+    group.each do |n|
+      color = 'white'
+      color = 'lightgray' if @graph.nodes_from(n).size == 0
+      @nodes[n] = c.add_node(n.to_s,
+        :style => "filled", :fillcolor => color)
+    end
+
+#    @viz.add_graph(label, :label => label, :style => 'filled', :rank => 'same'){ |c|
+#      group.each do |n|
+#        color = 'white'
+#        color = 'lightgray' if @graph.nodes_from(n).size == 0
+#        @nodes[n] = c.add_node(n.to_s,
+#          :style => "filled", :fillcolor => color)
+#      end
+#    }
   end
 
   draw_nodes(hci, "lab #1")
@@ -47,29 +59,32 @@ sample.map{ |s| "./net/#{s}.txt"}.each do |path|
   colors = ["green", "blue", "red", "purple"]
   clique_names = %w[cliques pseudo isolated ipc]
 
-#  @viz.subgraph{ |sub|
-#    sub[:rank => "same"]
-#    sub[:root => "true"]
-    cliques.zip(colors, clique_names).each do |pair|
-      pair[0].each_with_index do |c, idx|
-        root = @viz.add_node("#{pair[2]+idx.to_s}",
-          :shape => "diamond",
-          :style => "filled",
-          :fillcolor => pair[1])
-        root[:root] = true
-        c.each do |n|
-          @viz.add_edge(root, @nodes[n],
-            :color => pair[1],
-            :fontcolor => pair[1])
-        end
+  #  @viz.marks{ |sub|
+  #    sub[:rank => "same"]
+  #    sub[:root => "true"]
+  cliques.zip(colors, clique_names).each do |pair|
+    next if pair[0] == nil
+    pair[0].each_with_index do |c, idx|
+      mark = @viz.add_node("#{pair[2]+idx.to_s}",
+        :shape => "diamond",
+        :style => "filled",
+        :fillcolor => pair[1])
+      mark[:root] = true
+      c.each do |n|
+        @viz.add_edge(mark, @nodes[n],
+          :color => pair[1],
+          :fontcolor => pair[1])
       end
     end
-#  }
+  end
+  #  }
 
   print @viz.output(:canon => String)
   output_name = File.basename(path, ".*")
-  @viz.output(:use => :dot, :png => "./"+output_name+".png")
-  @viz.output(:use => :circo, :png => "./"+output_name+"_c.png")
-  @viz.output(:use => :twopi, :png => "./"+output_name+"_t.png")
+  @viz.output(:use => :dot, :png => "./clique_graph/"+output_name+".png")
+  @viz.output(:use => :neato, :png => "./clique_graph/"+output_name+"_n.png")
+  @viz.output(:use => :fdp, :png => "./clique_graph/"+output_name+"_f.png")
+  @viz.output(:use => :circo, :png => "./clique_graph/"+output_name+"_c.png")
+  @viz.output(:use => :twopi, :png => "./clique_graph/"+output_name+"_t.png")
 end
 
