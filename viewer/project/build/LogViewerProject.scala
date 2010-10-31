ab21cf7585a7da94d917ae1b1cabb3440a47f8f0
pokutuna <oneetyan@gmail.com>
2010-11-01 05:31:33 +0900

    ensime & sqlite test

--- viewer/project/build/LogViewerProject.scala
+++ viewer/project/build/LogViewerProject.scala
@@ -1,5 +1,8 @@
 import sbt._
 
 class LogViewerProject(info: ProjectInfo) extends DefaultProject(info){
+  override def compileOptions = super.compileOptions ++ compileOptions("-encoding", "utf8")
   val sqliteJDBC = "org.xerial" % "sqlite-jdbc" % "3.7.2"
+  val scalaTest = "org.scalatest" % "scalatest" % "1.2"
+  val orbroker = "orbroker" % "orbroker"
 }
