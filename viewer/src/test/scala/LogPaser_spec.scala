e58def4f3eb505f2aa5a24542e6c097791afdd2d
pokutuna <oneetyan@gmail.com>
2010-11-16 00:59:47 +0900

    test

---
+++ viewer/src/test/scala/LogPaser_spec.scala
@@ -0,0 +1,9 @@
+import org.btproject.test.SpecHelper
+import org.btproject.model._
+
+class LogFilePaserSpec extends SpecHelper {
+  describe("Paser"){
+    it("should search logedby")(pending)
+    it("should return unknown BDA when notfound logedby ")(pending)
+  }
+}
