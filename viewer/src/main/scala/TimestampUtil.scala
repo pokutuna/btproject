020c6f70b025362878063b7b2ba97308c13ebf09
pokutuna <oneetyan@gmail.com>
2010-11-15 16:28:06 +0900

    improve TimestampUtil

--- viewer/src/main/scala/TimestampUtil.scala
+++ viewer/src/main/scala/TimestampUtil.scala
@@ -5,25 +5,22 @@ import java.util.Calendar
 import java.util.Date
 import java.sql.Timestamp
 
-trait TimestampUtil { 
+object TimestampUtil {
   val sdf = new SimpleDateFormat("yyy/MM/dd kk:mm:ss")
-  
-  implicit def stringToTimestamp(s:String): Timestamp = {
-    val d = sdf.parse(s)
+  def parse(str:String):Timestamp = {
+    val d = sdf.parse(str)
     new Timestamp(d.getTime)
   }
-  
-  implicit def timestampToString(t:Timestamp): String = {
-    sdf.format(new Date(t.getTime))
-  }
-  
-  def strToOptionTimestamp(s:String): Option[Timestamp] = {
-    try {
-      val d = sdf.parse(s)
-      Some(new Timestamp(d.getTime))
-    } catch {
-      case e => None
-    }
-  }
 
+  def format(ts:Timestamp):String = {
+    sdf.format(new Date(ts.getTime))
+  }
+}
+
+trait TimestampUtil { 
+  implicit def stringToTimestamp(s:String): Timestamp =
+    TimestampUtil.parse(s)
+  
+  implicit def timestampToString(t:Timestamp): String = 
+    TimestampUtil.format(t)
 }
