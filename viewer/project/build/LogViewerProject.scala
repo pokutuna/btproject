214d1a22245e3bd849a466b6865ff473c6ab9f7f
pokutuna <oneetyan@gmail.com>
2010-11-16 02:31:12 +0900

    download scala-swing

--- viewer/project/build/LogViewerProject.scala
+++ viewer/project/build/LogViewerProject.scala
@@ -9,7 +9,8 @@ class LogViewerProject(info: ProjectInfo) extends DefaultProject(info){
   val h2DataBaseEngine = "com.h2database" % "h2" % "1.2.144"
   val scalaQuery = "org.scalaquery" % "scalaquery" % "0.9.0" from "http://scala-tools.org/repo-releases/org/scalaquery/scalaquery_2.8.0/0.9.0/scalaquery_2.8.0-0.9.0.jar"
   //val scalaQuery = "org.scalaquery" % "scalaquery" % "0.9.0"
-
+  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
+  
   import Process._
   lazy val syncLogData = task { "sh ./logdata_root/sync.sh" !; None}
   lazy val downloadJung = task { "sh ./lib/download_jung2.sh" !; None}
