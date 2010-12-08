8fc92564eb631c0e3b95fff85d36aceb0709c2ff
pokutuna <oneetyan@gmail.com>
2010-12-08 12:29:33 +0900

    うにょ

---
+++ viewer/src/main/scala/CliqueExtractor.scala
@@ -0,0 +1,36 @@
+package org.btproject.graph
+
+import collection.JavaConversions._
+import edu.uci.ics.jung.graph._
+import org.btproject.graph._
+import org.btproject.util._
+
+//trait CliqueExtractor[V,E] extends Graph[V,E]{
+
+class CliqueExtractor[V,E](graph:Graph[V,E]) { 
+
+  def isClique(nodes:Iterable[V]):Boolean = {
+    val nodeSet = nodes.toSet
+    nodeSet.foreach { v =>
+      val links:Set[V] = graph.getSuccessors(v).toSet
+      if (!(nodeSet - v).subsetOf(links)) return false
+    }
+    return true
+  }
+
+
+  def applyCombination(size:Int, nodes:Iterable[V])(f: Iterable[V] => Boolean):Iterable[Set[V]] = {
+    val res = Combination(size, nodes).filter(f(_)).map(_.toSet)
+    println(res.toList)
+    return res
+  }
+
+  def localMaximums(minSize:Int, nodes:Iterable[V]):Iterable[Set[V]] = {
+    if (minSize > nodes.size) return List()
+    val cliques = (minSize to nodes.size toList).flatMap{ size => 
+      applyCombination(size, nodes)(isClique(_))
+    }
+    
+    cliques
+  }
+}
