d114f8c63727babdc6f40dd3fc7d32b589fe455d
pokutuna <oneetyan@gmail.com>
2010-12-08 10:18:31 +0900

    oh my directory space charactor

--- misc/sync_logdata.sh
+++ misc/sync_logdata.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#OUT_DIR="~/Dropbox/LifeLog\ Project/"
-rsync -avz --stats tokuami@ns.hcilab.jp:~/human_network/ ${HOME}/log/
+rsync -avz --stats tokuami@ns.hcilab.jp:~/human_network/ ${HOME}'/Dropbox/LifeLog Project/LogServerMirror/'
