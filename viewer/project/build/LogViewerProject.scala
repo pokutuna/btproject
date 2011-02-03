d2e445809dc4f740c10b7a7c7c74ea21a27a8dbe
pokutuna <oneetyan@gmail.com>
2011-02-03 10:38:20 +0900

    update scala-query to 0.9.1

--- viewer/project/build/LogViewerProject.scala
+++ viewer/project/build/LogViewerProject.scala
@@ -7,8 +7,9 @@ class LogViewerProject(info: ProjectInfo) extends DefaultProject(info){
   //libraries
   val scalaTest = "org.scalatest" % "scalatest" % "1.2"
   val h2DataBaseEngine = "com.h2database" % "h2" % "1.2.144"
-  val scalaQuery = "org.scalaquery" % "scalaquery" % "0.9.0" from "http://scala-tools.org/repo-releases/org/scalaquery/scalaquery_2.8.0/0.9.0/scalaquery_2.8.0-0.9.0.jar"
-  //val scalaQuery = "org.scalaquery" % "scalaquery" % "0.9.0"
+  //val scalaQuery = "org.scalaquery" % "scalaquery" % "0.9.0" from 
+"http://scala-tools.org/repo-releases/org/scalaquery/scalaquery_2.8.0/0.9.0/scalaquery_2.8.0-0.9.0.jar"
+  val scalaQuery = "org.scalaquery" %% "scalaquery" % "0.9.1"
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
   
   import Process._
